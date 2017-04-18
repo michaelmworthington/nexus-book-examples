56cd6d1418ad41c5d2a8859e53e418d24d900244
Michael Worthington <michael.m.worthington@gmail.com>
2017-04-17 20:48:18 -0400

    adding example to input and output json and list all the repos in nexus. also, pulled in kelly's change to fix the grapes dependencies

---
+++ scripting/nexus-script-example/src/main/groovy/jsontest.groovy
@@ -0,0 +1,8 @@
+import groovy.json.JsonSlurper
+import groovy.json.JsonOutput
+
+//expects json string with appropriate content to be passed in
+def role = new JsonSlurper().parseText(args)
+
+return JsonOutput.toJson([result: 'Success!',
+                          message: role.id + " - " + role.name + " - " + role.description])
